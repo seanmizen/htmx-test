3dd3a1737672523e5a22cade66d633567d643212
seanmizen <sean@seanmizen.com>
2023-11-02 09:28:00 +0000

    clean up BE

--- server/server.ts
+++ server/server.ts
@@ -4,20 +4,39 @@ import path from "path";
 const app = express();
 const PORT = 8765;
 
-app.get("/some-api-endpoint", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 // Serve htmx from node_modules -> get it via <script src="/htmx/htmx.min.js"></script>
 app.use(
   "/htmx",
   express.static(path.join(__dirname, "../node_modules/htmx.org/dist"))
 );
 
-// serve FE
+// Serve FE
 app.use(express.static(path.join(__dirname, "../public")));
-console.log(`FE available at http://localhost:${PORT}/`);
+
+app.get("/some-api-endpoint", (req, res) => {
+  // res.json({ message: "Hello from server!" });
+  res.send(`
+    <h1>Server</h1>
+    <p>Hello from server!</p>
+    <a href="/">back</a>
+  `);
+});
+
+app.get("/blog", (_req, res) => {
+  res.send(`
+    <h1>Blog</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+    <a href="/">back</a>
+  `);
+});
+
+// Catch-all should come last
+app.get("/*", (_req, res) => {
+  res.status(404);
+  res.json({ message: "oh no!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Site available at http://localhost:${PORT}/`);
+});
